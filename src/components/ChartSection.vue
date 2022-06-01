3564b4ccefc5a145412e42dd308740e8578be31e
Max Baravy <maximkatut@gmail.com>
2022-06-01 13:31:44 -0400

    fix data attributes on labels

--- src/components/ChartSection.vue
+++ src/components/ChartSection.vue
@@ -5,31 +5,31 @@
     <ul class="chart-list">
       <li class="chart-item">
         <input id="mon" type="radio" name="weekday" />
-        <label for="mon" before="mon" after="$52.36"></label>
+        <label for="mon" data-before="mon" data-after="$34.55"></label>
       </li>
       <li class="chart-item">
         <input id="tue" type="radio" name="weekday" />
-        <label for="tue" before="tue"></label>
+        <label for="tue" data-before="tue"></label>
       </li>
       <li class="chart-item">
         <input id="wed" type="radio" name="weekday" checked />
-        <label for="wed" before="wed"></label>
+        <label for="wed" data-before="wed"></label>
       </li>
       <li class="chart-item">
         <input id="thu" type="radio" name="weekday" />
-        <label for="thu" before="thu"> </label>
+        <label for="thu" data-before="thu"> </label>
       </li>
       <li class="chart-item">
         <input id="fri" type="radio" name="weekday" />
-        <label for="fri" before="fri"> </label>
+        <label for="fri" data-before="fri"> </label>
       </li>
       <li class="chart-item">
         <input id="sat" type="radio" name="weekday" />
-        <label for="sat" before="sat"> </label>
+        <label for="sat" data-before="sat"> </label>
       </li>
       <li class="chart-item">
         <input id="sun" type="radio" name="weekday" />
-        <label for="sun" before="sun"> </label>
+        <label for="sun" data-before="sun"> </label>
       </li>
     </ul>
   </div>
