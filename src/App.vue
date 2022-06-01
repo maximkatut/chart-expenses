886ed8b17dc7346974b4ea6482c9cf9f7a6c9a5e
Max Baravy <maximkatut@gmail.com>
2022-06-01 11:31:55 -0400

    add components and styles files

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import BalanceSection from './components/BalanceSection.vue'
+// import ChartSection from './components/ChartSection.vue'
+// import TotalSection from './components/TotalSection.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="wrapper">
+    <BalanceSection />
+    <!-- <ChartSection/> -->
+    <!-- <TotalSection/> -->
+    <!-- <HelloWorld msg="hello" /> -->
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import './styles/main.scss';
 </style>
